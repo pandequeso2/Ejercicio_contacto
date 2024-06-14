0049dbf883b528eccc580b4ac54e3c2fb88e0eef
Benjamin <ben.arayag@duocuc.cl>
2024-06-14 13:20:31 -0400

    Menú listo

---
+++ ejercio_tipo_prueba_resuelto.py
@@ -0,0 +1,21 @@
+#Ejercicio tipo prueba 3
+import os,time
+
+while True:
+    os.system('cls')
+    print('Menú trabajador. ')
+    print('1: Registrar trabajador. ')
+    print('2: Listar todos los trabajadores. ')
+    print('3: imprimir panilla de suedos ')
+    print('4. Salir. ')
+    opc=int(input('Ingrese una opción: '))
+    os.system('cls')
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc==3:
+        pass
+    else:
+        print('Adioos') 
+    time.sleep(3) 
